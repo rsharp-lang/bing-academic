08a715a962f3184d5673fd5f4a3b5bca6974f4ce
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-02 16:35:27 +0800

    update unix man

--- academic/academic.vb
+++ academic/academic.vb
@@ -10,6 +10,7 @@ Imports Microsoft.VisualBasic.Language.C
 Public Module academic
 
     Const searchApiTemplate$ = "https://cn.bing.com/academic/search?q=%s&FORM=HDRSC4"
+    Const literatureProfileApiTemplate$ = "https://cn.bing.com/academic/profile?id=%s&encoded=0&v=paper_preview&mkt=zh-cn"
 
     ''' <summary>
     ''' Do bing academic term search
@@ -24,4 +25,19 @@ Public Module academic
         Return html
     End Function
 
+    ''' <summary>
+    ''' Get profile of the given literature entry
+    ''' </summary>
+    ''' <param name="literature">
+    ''' the literature term entry from the <see cref="search(String)"/> result.
+    ''' </param>
+    ''' <returns></returns>
+    <ExportAPI("profile.literature")>
+    Public Function profile(literature As literatureEntry)
+        Dim url As String = sprintf(literatureProfileApiTemplate, literature.guid.UrlEncode)
+        Dim html As String = url.GET
+
+        Return html
+    End Function
 End Module
+
---
+++ academic/models.vb
@@ -0,0 +1,9 @@
+﻿Public Class literature
+
+    Public Property guid As String
+End Class
+
+Public Class literatureEntry
+
+    Public Property guid As String
+End Class
