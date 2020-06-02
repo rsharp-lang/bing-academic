6ca0e8d96885564ce4d8b3c5a5534fbbcd0e52dd
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-02 18:08:24 +0800

    Update models.vb

--- academic/models.vb
+++ academic/models.vb
@@ -1,4 +1,6 @@
-﻿Public Class literature
+﻿Imports Microsoft.VisualBasic.Text.Parser.HtmlParser
+
+Public Class literature
 
     Public Property guid As String
 
@@ -21,7 +23,15 @@ Public Class literatureEntry
     End Function
 
     Friend Shared Function literatureEntry(html As String) As literatureEntry
+        Dim title = html.Match("<h\d+.+?</h\d+>", RegexICSng)
+        Dim summary As New literatureEntry With {
+            .title = title.StripHTMLTags,
+            .guid = title.href.Match("id[=].+?[&]", RegexICSng).GetTagValue("=").Value.Trim("&"c)
+        }
+        Dim div = html.Matches("<div.+?</div>", RegexICSng).ToArray
 
+
+        Return summary
     End Function
 
 End Class
