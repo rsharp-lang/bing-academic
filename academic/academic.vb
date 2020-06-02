c6b77d87ddeee42bb07b3e4f373fd937063c2e15
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-02 16:10:43 +0800

    config projects

---
+++ academic/academic.vb
@@ -0,0 +1,15 @@
+﻿
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("search")>
+Public Class academic
+
+    Const searchApiTemplate$ = "https://cn.bing.com/academic/search?q=%s&FORM=HDRSC4"
+
+    <ExportAPI("search")>
+    Public Function search(term As String)
+
+    End Function
+
+End Class
