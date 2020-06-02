c42d5dc39d41b8213a9b024941b96fefae8e3984
xieguigang <xie.guigang@gcmodeller.org>
2020-06-02 22:25:51 +0800

    Update models.vb

--- academic/models.vb
+++ academic/models.vb
@@ -1,4 +1,6 @@
-﻿Imports Microsoft.VisualBasic.Text.Parser.HtmlParser
+﻿Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Text
+Imports Microsoft.VisualBasic.Text.Parser.HtmlParser
 
 Public Class literature
 
@@ -25,13 +27,40 @@ Public Class literatureEntry
     Friend Shared Function literatureEntry(html As String) As literatureEntry
         Dim title = html.Match("<h\d+.+?</h\d+>", RegexICSng)
         Dim summary As New literatureEntry With {
-            .title = title.StripHTMLTags,
-            .guid = title.href.Match("id[=].+?[&]", RegexICSng).GetTagValue("=").Value.Trim("&"c)
+            .title = title.StripHTMLTags.Trim(" "c, ASCII.TAB, ASCII.CR, ASCII.LF),
+            .guid = title.href.DoCall(AddressOf getGuid)
         }
         Dim div = html.Matches("<div.+?</div>", RegexICSng).ToArray
-
+        Dim authors = div(Scan0) _
+            .Matches("<a.+?</a>", RegexICSng) _
+            .Select(Function(a)
+                        Return New author With {
+                            .guid = a.href.DoCall(AddressOf getGuid),
+                            .name = a.GetValue
+                        }
+                    End Function) _
+            .ToArray
 
         Return summary
+    End Function
+
+    Friend Shared Function getGuid(href As String) As String
+        Return href _
+            .Match("id[=].+", RegexICSng) _
+            .GetTagValue("=").Value _
+           ?.Split("&"c) _
+            .FirstOrDefault
+    End Function
+
+End Class
+
+Public Class author
+
+    Public Property name As String
+    Public Property guid As String
+
+    Public Overrides Function ToString() As String
+        Return name
     End Function
 
 End Class
