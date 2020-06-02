ea9f8aff6c8d48ceff6fd0682df5e40bd7f58fcb
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2020-06-02 17:52:05 +0800

    add test data

--- academic/academic.vb
+++ academic/academic.vb
@@ -2,6 +2,7 @@
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports Microsoft.VisualBasic.Language.C
+Imports Microsoft.VisualBasic.Text.Parser.HtmlParser
 
 ''' <summary>
 ''' Bing search for academic
@@ -21,6 +22,9 @@ Public Module academic
     Public Function search(term As String) As Object
         Dim url As String = sprintf(searchApiTemplate, term.UrlEncode)
         Dim html As String = url.GET
+
+        ' clean up html codes
+        html = html.RemovesJavaScript.RemovesCSSstyles
 
         Return html
     End Function
