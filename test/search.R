9e195ed61b1656e4c464aba690c119990418a3ae
xieguigang <xie.guigang@gcmodeller.org>
2021-08-14 11:52:34 +0800

    Update search.R

--- test/search.R
+++ test/search.R
@@ -1,12 +1,1 @@
-imports "search" from "../dist/academic.dll";
-
-let data = search("aes")
-:> as.data.frame
-;
-
-data
-:> print
-;
-data 
-:> write.csv(file = "../data/demo_search.csv")
-;
+print(BingAcademic::search("aes"));
